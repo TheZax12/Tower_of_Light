cca7b1b778ade0771f686728b45fc61bd23428a1
Vasilis Zachos <the.Zax12@gmail.com>
2025-02-28 04:46:44 +0200

    Update "Colors" file.

--- UI/Colors.py
+++ UI/Colors.py
@@ -22,17 +22,47 @@ chaos_floor_tile_visible_color = '#9ca3af'
 chaos_floor_tile_invisible_color = '#6b7280'
 """ Grey 500 """
 
-light_wall_tile_visible_color = '#78350f'
-""" Amber 900 """
+light_wall_tile_visible_color = '#713f12'
+""" Yellow 900 """
 
-light_wall_tile_invisible_color = '#451a03'
-""" Amber 950 """
+light_wall_tile_invisible_color = '#422006'
+""" Yellow 950 """
 
-light_floor_tile_visible_color = '#b45309'
-""" Amber 700 """
+light_floor_tile_visible_color = '#a16207'
+""" Yellow 700 """
 
-light_floor_tile_invisible_color = '#92400e'
-""" Amber 800 """
+light_floor_tile_invisible_color = '#854d0e'
+""" Yellow 800 """
+
+usable_tile_visible_color =  '#e879f9'
+""" Fuchsia 400 """
+
+usable_tile_invisible_color = '#a21caf'
+""" Fuchsia 700 """
+
+equipable_tile_visible_color = '#34d399'
+""" Emerald 400 """
+
+equipable_tile_invisible_color = '#059669'
+""" Emerald 600 """
+
+beacon_tile_visible_color = '#f97316'
+""" Orange 500 """
+
+beacon_tile_invisible_color = '#c2410c'
+""" Orange 800 """
+
+exit_tile_visible_color = '#fde047'
+""" Yellow 300 """
+
+exit_tile_invisible_color = '#fbbf24'
+""" Yellow 400 """
 
 player_color = '#84cc16'
 """ Lime 500 """
+
+enemy_visible_color = '#dc2626'
+""" Red 600 """
+
+enemy_invinsible_color = '#991b1b'
+""" Red 800 """
